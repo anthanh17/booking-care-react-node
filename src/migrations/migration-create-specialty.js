1500bf75acf05746b54c435c0e949976bfe12d2b
Annt <annguyenthe.work@gmail.com>
2023-08-22 23:03:02 +0700

    update models, migrations

--- src/migrations/migration-create-specialty.js
+++ src/migrations/migration-create-specialty.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
+      name: {
+        type: Sequelize.TEXT,
+      },
       image: {
         type: Sequelize.STRING,
       },
